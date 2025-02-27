9648d227b027c0961eb45e8170e3f13dbcb2a740
JaimePalM <jaimepalacios240@gmail.com>
2025-02-27 11:35:03 +0100

    test trigger 2

--- jenkins/HelloJenkins.java
+++ jenkins/HelloJenkins.java
@@ -1,9 +1,5 @@
 public class HelloJenkins {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello, Jenkins! " + i);
-        }
+        System.out.println("Hello, Jenkins! from GitHub");
     }
 }
